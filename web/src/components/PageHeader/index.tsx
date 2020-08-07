daa77c2566d623fcd74031b40194935443a02d09
fvvsantana <fvvsantana@gmail.com>
2020-08-07 20:46:25 +0000

    Create list of professors

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import logoImg from '../../assets/images/logo.svg';
 import backIcon from '../../assets/images/icons/back.svg';
 
+import './styles.css';
+
 interface PageHeaderProps{
     title: string;
 }
@@ -19,6 +21,8 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
             </div>
             <div className="header-content">
                 <strong>{props.title}</strong>
+
+                {props.children}
             </div>
         </header>
     )
